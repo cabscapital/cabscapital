40ad8e12f36665466f94ebfe8126d3ea05019500
cabscapital <32318653+cabscapital@users.noreply.github.com>
2017-10-02 10:47:48 -0500

    Actualización Base de Datos
    Se define base de datos por Google Cloud Platform

--- logic/db_config.php
+++ logic/db_config.php
@@ -2,5 +2,5 @@
 define('DB_USER', "admcabs"); // Usuario base de datos
 define('DB_PASSWORD', "CapCap25"); // Contraseña base de datos
 define('DB_DATABASE', "users"); // Nombre base de datos
-define('DB_SERVER', "cabscapital0.database.windows.net"); // Servidor base de datos 
+define('DB_SERVER', "x3usv2d6pzdblev6fjfxggj45a@speckle-umbrella-11.iam.gserviceaccount.com"); // Servidor base de datos 
 ?>

07128cec2cfa283e98142ffc8070aa97a2e10536
cabscapital <32318653+cabscapital@users.noreply.github.com>
2017-10-03 16:05:18 -0500

    Modificación DB server por IP

--- logic/db_config.php
+++ logic/db_config.php
@@ -2,5 +2,5 @@
 define('DB_USER', "admcabs"); // Usuario base de datos
 define('DB_PASSWORD', "CapCap25"); // Contraseña base de datos
 define('DB_DATABASE', "users"); // Nombre base de datos
-define('DB_SERVER', "x3usv2d6pzdblev6fjfxggj45a@speckle-umbrella-11.iam.gserviceaccount.com"); // Servidor base de datos 
+define('DB_SERVER', "35.192.104.21"); // Servidor base de datos 
 ?>

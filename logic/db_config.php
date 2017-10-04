194c53df4fa4ffeb6f4f34027ffb0c53252a27ad
cabscapital <32318653+cabscapital@users.noreply.github.com>
2017-10-03 22:53:09 -0500

    Update db_config.php

--- logic/db_config.php
+++ logic/db_config.php
@@ -1,6 +1,6 @@
 ﻿<?php
-define('DB_USER', "admcabs"); // Usuario base de datos
-define('DB_PASSWORD', "CapCap25"); // Contraseña base de datos
+define('DB_USER', "root@35.192.163.247"); // Usuario base de datos
+define('DB_PASSWORD', "SemiMolto100*"); // Contraseña base de datos
 define('DB_DATABASE', "users"); // Nombre base de datos
-define('DB_SERVER', "35.192.131.13:3306"); // Servidor base de datos 
+define('DB_SERVER', "35.192.131.13"); // Servidor base de datos 
 ?>
